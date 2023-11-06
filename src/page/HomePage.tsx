5dcd9013a9e3163e9623364591220ee1a2874f13
devfabien <ish001fabi@gmail.com>
2023-11-06 15:57:07 +0200

    adding the reset button

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,12 +1,13 @@
 export default function HomePage() {
   return (
-    <div className="bg-sky-700 px-10 py-20 flex flex-col justify-center items-center">
-      <h1 className="text-5xl text-white font-semibold py-3">Counter App</h1>
-      <div className="flex gap-20 px-7 font-medium items-center">
+    <div className="bg-sky-700 px-10 py-14 flex flex-col justify-center items-center  font-medium">
+      <h1 className="text-5xl text-white font-semibold py-10">Counter App</h1>
+      <div className="flex gap-20 px-7 items-center my-5">
         <button className="bg-white rounded-md p-3">Decrement</button>
         <p className="text-5xl py-6 px-10 bg-white rounded-full">0</p>
         <button className="bg-white rounded-md p-3">Increment</button>
       </div>
+      <button className="bg-white rounded-md py-3 px-7 my-4">Reset</button>
     </div>
   );
 }
